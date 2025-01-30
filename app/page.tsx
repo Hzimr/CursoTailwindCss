6f31f55e82d044dad4452c4943f9a941cb3f25dd
Hítalo Rodrigues <hitalo005@gmail.com>
2025-01-29 22:34:10 -0300

    feat: html inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import Cover from "../public/cover.png";
+
 export default function Home() {
-  return <div className="">Hello Curso!</div>;
+  return (
+    <main className="flex w-full h-screen items-center justify-center">
+      <div className="shadow-md box-border border-solid border-8 rounded-md">
+        <div className="">
+          <div>
+            <img className="size-48" src={Cover.src} alt="" />
+          </div>
+        </div>
+        <div>
+          <span>Class Warfare</span>
+          <span>The Anti-Patterns</span>
+          <span>
+            <span>No. 4</span>
+            <span>·</span>
+            <span>2025</span>
+          </span>
+        </div>
+      </div>
+    </main>
+  );
 }
